f1b72cfe869bdbe731489fbc110b0a4dfd2ad593
rohan <rohanverma031@gmail.com>
2025-02-28 17:56:00 +0530

    feat: added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/main.scss";
 import Authentication from "@/components/Authentication/Authentication";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<SpeedInsights />
 				<Authentication>{children}</Authentication>
 			</body>
 		</html>
