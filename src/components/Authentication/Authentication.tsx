289aaf245299010ef9155b45c0f7a806a5f6a3c4
rohan <rohanverma031@gmail.com>
2025-02-28 18:12:25 +0530

    update: create amplify getter

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -6,7 +6,6 @@ import {
 	ApolloClient,
 	InMemoryCache,
 	ApolloProvider,
-	gql,
 	createHttpLink,
 } from "@apollo/client";
 
@@ -17,13 +16,13 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
-import outputs from "../../amplifyconfiguration.json";
 import Nav from "../Nav/Nav";
 
-Amplify.configure(outputs);
+Amplify.configure(getAmplifyConfig());
 
 import { cognitoUserPoolsTokenProvider } from "aws-amplify/auth/cognito";
 import { sessionStorage } from "aws-amplify/utils";
+import { getAmplifyConfig } from "@/utils/create_amplify_config";
 
 cognitoUserPoolsTokenProvider.setKeyValueStorage(sessionStorage);
 
---
+++ src/utils/create_amplify_config.ts
@@ -0,0 +1,34 @@
+export const getAmplifyConfig = () => {
+	const identityPoolId = process.env.IDENTITY_POOL_ID;
+	const userPoolId = process.env.USER_POOL_ID;
+	const clientId = process.env.CLIENT_ID;
+	const domain = process.env.DOMAIN;
+
+	return {
+		aws_project_region: "ap-south-1",
+		aws_cognito_identity_pool_id: identityPoolId,
+		aws_cognito_region: "ap-south-1",
+		aws_user_pools_id: userPoolId,
+		aws_user_pools_web_client_id: clientId,
+		oauth: {
+			domain: domain,
+		},
+		aws_cognito_username_attributes: ["EMAIL"],
+		aws_cognito_social_providers: [],
+		aws_cognito_signup_attributes: [],
+		aws_cognito_mfa_configuration: "OFF",
+		aws_cognito_mfa_types: [],
+		aws_cognito_password_protection_settings: {
+			passwordPolicyMinLength: 8,
+			passwordPolicyCharacters: [
+				"REQUIRES_LOWERCASE",
+				"REQUIRES_UPPERCASE",
+				"REQUIRES_NUMBERS",
+				"REQUIRES_SYMBOLS",
+			],
+		},
+		aws_cognito_verification_mechanisms: ["EMAIL"],
+		aws_user_files_s3_bucket: "anant-objects",
+		aws_user_files_s3_bucket_region: "ap-south-1",
+	};
+};
