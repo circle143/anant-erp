4fd63cf40608bc19bc4e456e30da9f05f39c13b4
rohan <rohanverma031@gmail.com>
2025-04-21 11:51:14 +0530

    update: updated amdin create org request body

--- src/redux/action/admin.tsx
+++ src/redux/action/admin.tsx
@@ -3,14 +3,14 @@ import { getIdToken } from "@/utils/get_user_tokens";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 // Function to add an organization admin user
-export const addOrgAdminUser = async (Name: string, RootUserEmail: string) => {
+export const addOrgAdminUser = async (Name: string, Email: string) => {
 	try {
 		const token = await getIdToken();
 		const response = await axios.post(
 			`${apiUrl}/organization`,
 			{
 				Name,
-				RootUserEmail,
+				Email,
 			},
 			{
 				headers: {
@@ -30,45 +30,45 @@ export const addOrgAdminUser = async (Name: string, RootUserEmail: string) => {
 };
 
 export const getOrg = async (cursor: string | null = null) => {
-  try {
-    const token = await getIdToken(); // fetch token each time
-    const response = await axios.get(
-      `${apiUrl}/organization${cursor ? `?cursor=${cursor}` : ""}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return response.data;
-  } catch (error: any) {
-    console.error(
-      "Error fetching orgs:",
-      error.response?.data || error.message
-    );
-    return { error: true, message: error.message };
-  }
+	try {
+		const token = await getIdToken(); // fetch token each time
+		const response = await axios.get(
+			`${apiUrl}/organization${cursor ? `?cursor=${cursor}` : ""}`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		return response.data;
+	} catch (error: any) {
+		console.error(
+			"Error fetching orgs:",
+			error.response?.data || error.message
+		);
+		return { error: true, message: error.message };
+	}
 };
 
 export const updateStatus = async (orgId: string, status: string) => {
-  try {
-    const token = await getIdToken(); // fetch token each time
-    const response = await axios.patch(
-      `${apiUrl}/organization/${orgId}/status`,
-      { status },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json", 
-        },
-      }
-    );
-    return response.data;
-  } catch (error: any) {
-    console.error(
-      "Error in updating org status:",
-      error.response?.data || error.message
-    );
-    return { error: true, message: error.message };
-  }
+	try {
+		const token = await getIdToken(); // fetch token each time
+		const response = await axios.patch(
+			`${apiUrl}/organization/${orgId}/status`,
+			{ status },
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		return response.data;
+	} catch (error: any) {
+		console.error(
+			"Error in updating org status:",
+			error.response?.data || error.message
+		);
+		return { error: true, message: error.message };
+	}
 };
