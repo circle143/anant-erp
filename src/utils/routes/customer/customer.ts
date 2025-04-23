c0839bd8329de361417a1a239d34242950357b68
rohan <rohanverma031@gmail.com>
2025-04-23 09:30:27 +0530

    feat: added customer endpoints

---
+++ src/utils/routes/customer/customer.ts
@@ -0,0 +1,31 @@
+import {
+	AddCustomerToFlatInput,
+	AddCustomerToFlatRequestBodyInput,
+	UpdateCustomerInput,
+	UpdateCustomerRequestBodyInput,
+} from "./types";
+
+function getBasePath(societyReraNumber: string, flatId: string) {
+	return `/society/${societyReraNumber}/flat/${flatId}/customer`;
+}
+
+export const customer = {
+	addCustomerToFlat: {
+		getEndpoint: (input: AddCustomerToFlatInput) => {
+			return getBasePath(input.societyReraNumber, input.flatID);
+		},
+		getReqBody: (input: AddCustomerToFlatRequestBodyInput) => {
+			return JSON.stringify(input);
+		},
+		requestMethod: "POST",
+	},
+	updateCustomerToFlat: {
+		getEndpoint: (input: UpdateCustomerInput) => {
+			return getBasePath(input.societyReraNumber, input.flatID);
+		},
+		getReqBody: (input: UpdateCustomerRequestBodyInput) => {
+			return JSON.stringify(input);
+		},
+		requestMethod: "POST",
+	},
+};
---
+++ src/utils/routes/customer/types.ts
@@ -0,0 +1,44 @@
+export interface CustomerDetails {
+	level: number;
+	salutation: string;
+	firstName: string;
+	lastName: string;
+	dateOfBirth: Date; // ISO string representation of time.Time
+	gender: string;
+	photo: string;
+	maritalStatus: string;
+	nationality: string;
+	email: string;
+	phoneNumber: string; // e164 format
+
+	middleName?: string;
+	numberOfChildren?: number;
+	anniversaryDate?: Date; // ISO string representation of time.Time
+	aadharNumber?: string;
+	panNumber?: string;
+	passportNumber?: string;
+	profession?: string;
+	designation?: string;
+	companyName?: string;
+}
+
+// add customers to flat
+export interface AddCustomerToFlatInput {
+	societyReraNumber: string;
+	flatID: string;
+}
+
+export interface AddCustomerToFlatRequestBodyInput {
+	details: CustomerDetails[];
+}
+
+// update customer
+export interface UpdateCustomerInput {
+	societyReraNumber: string;
+	flatID: string;
+	customerID: string;
+}
+
+export interface UpdateCustomerRequestBodyInput {
+	details: CustomerDetails;
+}
