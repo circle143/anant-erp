f9df3c154a02090b1e13fc38ac9f6edb9f4793fa
rohan <rohanverma031@gmail.com>
2025-05-28 14:13:35 +0530

    feat: added bank report endpoint

--- src/utils/routes/bank/bank.ts
+++ src/utils/routes/bank/bank.ts
@@ -1,6 +1,7 @@
 import {
 	BankByIdRouteInput,
 	BankDetailsReqBodyInput,
+	BankReportReqBody,
 	BankRouteInput,
 } from "./types";
 
@@ -9,34 +10,43 @@ function getBasePath(societyReraNumber: string) {
 }
 
 export const bank = {
-  addBank: {
-    getEndpoint: (input: BankRouteInput) => {
-      return getBasePath(input.societyRera);
-    },
-    getReqBody: (input: BankDetailsReqBodyInput) => {
-      return input;
-    },
-    requestMethod: "POST",
-  },
-  updateBankDetails: {
-    getEndpoint: (input: BankByIdRouteInput) => {
-      return getBasePath(input.societyRera) + `/${input.bankId}`;
-    },
-    getReqBody: (input: BankDetailsReqBodyInput) => {
-      return input;
-    },
-    requestMethod: "PATCH",
-  },
+	addBank: {
+		getEndpoint: (input: BankRouteInput) => {
+			return getBasePath(input.societyRera);
+		},
+		getReqBody: (input: BankDetailsReqBodyInput) => {
+			return input;
+		},
+		requestMethod: "POST",
+	},
+	updateBankDetails: {
+		getEndpoint: (input: BankByIdRouteInput) => {
+			return getBasePath(input.societyRera) + `/${input.bankId}`;
+		},
+		getReqBody: (input: BankDetailsReqBodyInput) => {
+			return input;
+		},
+		requestMethod: "PATCH",
+	},
 
-  getAllSocietyBanks: {
-    getEndpoint: (input: BankRouteInput) => {
-      if (!input.cursor || input.cursor.trim().length == 0)
-        return getBasePath(input.societyRera);
-      return getBasePath(input.societyRera) + `?cursor=${input.cursor}`;
-    },
-    getReqBody: () => {
-      // no request body required
-    },
-    requestMethod: "GET",
-  },
+	getAllSocietyBanks: {
+		getEndpoint: (input: BankRouteInput) => {
+			if (!input.cursor || input.cursor.trim().length == 0)
+				return getBasePath(input.societyRera);
+			return getBasePath(input.societyRera) + `?cursor=${input.cursor}`;
+		},
+		getReqBody: () => {
+			// no request body required
+		},
+		requestMethod: "GET",
+	},
+	getBankReport: {
+		getEndpoint: (input: BankByIdRouteInput) => {
+			return getBasePath(input.societyRera) + `/${input.bankId}/report`;
+		},
+		getReqBody: (input: BankReportReqBody) => {
+			return input;
+		},
+		requestMethod: "POST",
+	},
 };
