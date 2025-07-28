681e798bc11555ee0b687de41b1fa60967fcddde
anirudh <anirudh.saini852@gmail.com>
2025-07-28 23:16:51 +0530

    fixed build

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Spinner from "react-bootstrap/Spinner";
 import styles from "./loader.module.scss";
 
 const Loader = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true); // Ensures this only runs on the client
+  }, []);
+
+  if (!mounted) return null;
+
   return ReactDOM.createPortal(
     <div className={styles.loader}>
       <Spinner animation="border" />
     </div>,
-    document.body // ⬅️ Appends directly to the body
+    document.body
   );
 };
 
